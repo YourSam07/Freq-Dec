47b21168d89bfe8647887e8ee4329609c7aacdd0
Sankalp Sethi <54805355+YourSam07@users.noreply.github.com>
2024-06-20 13:09:43 -0700

    next page feature

--- getdata.py
+++ getdata.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -63,22 +64,34 @@ def main():
     print("Connected")
     
     login(driver)
-        
-    searchInput = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, "search-sounds"))
-    )
-    searchInput.send_keys(searchTerms[0])
-    searchInput.send_keys(Keys.RETURN)
+    for searchTerm in searchTerms:
+      searchInput = WebDriverWait(driver, 10).until(
+          EC.element_to_be_clickable((By.ID, "search-sounds"))
+      )
+      searchInput.send_keys(searchTerm)
+      searchInput.send_keys(Keys.RETURN)
 
-    audioElements = driver.find_elements(By.CLASS_NAME, "bw-search__result")
-    print("Audio Elements", audioElements)
+      while True: 
+        audioElements = driver.find_elements(By.CLASS_NAME, "bw-search__result")
+        print("Audio Elements", audioElements)
 
-    for ele in audioElements: 
-      duration = ele.find_element(By.CLASS_NAME, "bw-total__sound_duration")
-      if float(duration.text.split(":")[0]) < 1.0:
-        srcFile = ele.find_element(By.XPATH, ".//source[@type='audio/ogg']")
-        # print("Source", srcFile.get_attribute("src"))
-        downloadSample(srcFile.get_attribute("src"))
+        for ele in audioElements: 
+          duration = ele.find_element(By.CLASS_NAME, "bw-total__sound_duration")
+          if float(duration.text.split(":")[0]) < 1.0:
+            srcFile = ele.find_element(By.XPATH, ".//source[@type='audio/ogg']")
+            # print("Source", srcFile.get_attribute("src"))
+            downloadSample(srcFile.get_attribute("src"))
+
+        try:
+          nextBtn = driver.find_element(By.XPATH, "//a[@title='Next Page']")
+          driver.execute_script("arguments[0].scrollIntoView(true);", nextBtn)  # Scroll the button into view
+          time.sleep(5)  # Wait a moment for the scroll action to complete
+          nextBtn.click()
+          time.sleep(3)
+        except Exception as error:
+          print("No more pages to navigate.")
+          print(f"Error: {error}")
+          break
 
     input("Press a key to exit....\n")
 
