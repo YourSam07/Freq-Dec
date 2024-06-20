538f91a59061a282969d8cba33227544c60f1df2
Sankalp Sethi <54805355+YourSam07@users.noreply.github.com>
2024-06-20 12:28:16 -0700

    downloading done

--- getdata.py
+++ getdata.py
@@ -1,57 +1,13 @@
+import os
+import json
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-import json
 
-with open('creds.json', 'r') as jsonFile:
-  credentials = json.load(jsonFile)
-
-def main():
-  url = "https://freesound.org/"
-  driver = webdriver.Chrome()
-  searchTerms =  ["microphone feedback", "mic feedback", "audio feedback", "live sound feedback", "PA feedback"]
-
-  try:
-    driver.get(url)
-    print("Connected")
-    
-    login(driver)
-        
-    searchInput = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, "search-sounds"))
-    )
-    searchInput.send_keys(searchTerms[0])
-    searchInput.send_keys(Keys.RETURN)
-
-    audioElements = driver.find_elements(By.CLASS_NAME, "bw-search__result")
-    print(audioElements)
-
-    for ele in audioElements: 
-      duration = ele.find_element(By.CLASS_NAME, "bw-total__sound_duration")
-      if float(duration.text.split(":")[0]) < 1.0:
-        name = ele.find_element(By.CLASS_NAME, "bw-link--black")
-        name.click()
-        
-        WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "sound-download-button")))
-        
-        download = driver.find_element(By.CLASS_NAME, "sound-download-button")
-        download.click()
-        
-        print("Downloading... ")
-        driver.back()
-        print("Download finsihed.")
-
-    input("Press a key to exit....")
-
-
-  finally:
-    driver.quit()
-    
-    
 def login(d):
   try:
     login_button = WebDriverWait(d, 10).until(
@@ -71,6 +27,67 @@ def login(d):
   except:
     print("Login Failed")
   
+  
+def downloadSample(url):
+  print(url)
+  if not os.path.exists("sound_samples"):
+    os.makedirs("sound_samples")
+        
+  # Extract the filename from the URL
+  filename = url.split("/")[-1]
+  file_path = os.path.join("sound_samples", filename)
+  
+  try:
+    # Send a GET request to the URL
+    response = requests.get(url)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+      # Write the content to a file
+      with open(file_path, 'wb') as file:
+        file.write(response.content)
+      print(f"File downloaded successfully and saved to {file_path}")
+    else:
+      print(f"Failed to download file. HTTP Status Code: {response.status_code}")
+  except Exception as e:
+      print(f"An error occurred: {e}")
+      
+
+def main():
+  url = "https://freesound.org/"
+  driver = webdriver.Chrome()
+  searchTerms =  ["microphone feedback", "mic feedback", "audio feedback", "live sound feedback", "PA feedback"]
+
+  try:
+    driver.get(url)
+    print("Connected")
+    
+    login(driver)
+        
+    searchInput = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "search-sounds"))
+    )
+    searchInput.send_keys(searchTerms[0])
+    searchInput.send_keys(Keys.RETURN)
+
+    audioElements = driver.find_elements(By.CLASS_NAME, "bw-search__result")
+    print("Audio Elements", audioElements)
+
+    for ele in audioElements: 
+      duration = ele.find_element(By.CLASS_NAME, "bw-total__sound_duration")
+      if float(duration.text.split(":")[0]) < 1.0:
+        srcFile = ele.find_element(By.XPATH, ".//source[@type='audio/ogg']")
+        # print("Source", srcFile.get_attribute("src"))
+        downloadSample(srcFile.get_attribute("src"))
+
+    input("Press a key to exit....\n")
+
+  finally:
+    driver.quit()
 
 if __name__ == "__main__":
+  
+  with open('creds.json', 'r') as jsonFile:
+    credentials = json.load(jsonFile)
+  
   main()
